a0e31df345429afc1d748110d08e10d530781796
lazyTimes <1097483508@qq.com>
2019-08-18 20:09:42 +0800

    测试提交

---
+++ cunple-app/src/main/java/com/myapp/cunpleapp/CunpleAppApplication.java
@@ -0,0 +1,15 @@
+package com.myapp.cunpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunpleAppApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CunpleAppApplication.class, args);
+    }
+
+}
